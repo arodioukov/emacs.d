67cb935b21a161d25db413dc37d3d6dedc1e7f00
Alex Rodioukov <alex.rodioukov@outpace.com>
2014-12-01 18:07:56 -0700

    solarized tweaks

--- theme.el
+++ theme.el
@@ -5,3 +5,12 @@
 (set-frame-font "Inconsolata-16")
 
 (load-theme 'solarized-dark t)
+
+;; make the fringe stand out from the background
+(setq solarized-distinct-fringe-background t)
+;; make the modeline high contrast
+(setq solarized-high-contrast-mode-line t)
+;; Use less bolding
+(setq solarized-use-less-bold t)
+;; Don't change size of org-mode headlines (but keep other size-changes)
+(setq solarized-scale-org-headlines nil)
